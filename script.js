177e2867a3fa19ecc7380f87dd3675ec1d0bb200
Krzysztof Duś <krecik11120@gmail.com>
2016-08-20 21:47:37 +0200

    add ;

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ Telefon.prototype.printInfo = function() {
    console.log('Marka telefonu to ' + this.marka + ', kolor to ' + this.kolor + ', a cena to ' + this.cena + '.'); 
 };
 
-var SamsungS6 = new Telefon("Samsung", 2000, "zloty") 
-var OnePlus  = new Telefon("Htc", 1500, "bialy") 
-var iPhone6s = new Telefon("Apple", 2500, "srebrny") 
+var SamsungS6 = new Telefon("Samsung", 2000, "zloty"); 
+var OnePlus  = new Telefon("Htc", 1500, "bialy"); 
+var iPhone6s = new Telefon("Apple", 2500, "srebrny"); 
 iPhone6s.printInfo();
 OnePlus.printInfo();
 SamsungS6.printInfo();
